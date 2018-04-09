f1992723eefdc9f5e938af8eb30ebd1fae3542e8
Gusti Nurshobah <shobah.app01@gmail.com>
2018-04-09 18:31:59 +0800

    Intstance, buat atribut dan panggil method

--- Praktekk03/src/praktekk03/LaptopAksi.java
+++ Praktekk03/src/praktekk03/LaptopAksi.java
@@ -3,6 +3,11 @@ package praktekk03;
 
 public class LaptopAksi {
     public static void main(String[] args) {
+        Laptop l = new Laptop();
+        l.Merk="Acer";
+        l.Warna="Merah";
+        l.Harga= "3500000" ;
         
+        l.cetakInfo();
     }
 }

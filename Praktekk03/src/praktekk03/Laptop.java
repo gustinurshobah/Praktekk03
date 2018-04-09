c6cc697c01b40d4ee78b04d882d27c06948b9743
Gusti Nurshobah <shobah.app01@gmail.com>
2018-04-09 18:24:21 +0800

    Membuat Method Cetakinfo()

--- Praktekk03/src/praktekk03/Laptop.java
+++ Praktekk03/src/praktekk03/Laptop.java
@@ -5,4 +5,10 @@ public class Laptop {
     String Merk;
     String Warna;
     String Harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+Merk+"\n"+
+                "Warna \t: "+Warna+"\n"+
+                "Harga \t: "+Harga);
+    }
 }
